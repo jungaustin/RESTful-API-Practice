469810d4aba7dc2f0a1f64772bf9ade8b6e8b85c
AusJung <137535372+jungaustin@users.noreply.github.com>
2025-02-14 14:27:59 -0800

    Changed Input to Pydantic Models

--- main.py
+++ main.py
@@ -3,22 +3,27 @@ from pydantic import BaseModel
 app = FastAPI()
 
 items = []
+#If I want an Item required, I could remove "= None" so it fails to run if I don't have it. Otherwise i can leave it if its optional
+class Item(BaseModel):
+    text: str = None
+    is_done: bool = False
+
 
 @app.get("/")
 def root():
     return {"Hello": "World"}
 
 @app.post("/items")
-def create_item(item: str):
+def create_item(item: Item):
     items.append(item)
     return items
 
-@app.get("/items")
+@app.get("/items", response_model=list[Item])
 def list_items(limit: int = 10):
     return items[0:limit]
 
-@app.get("/items/{item_id}")
-def get_item(item_id:int):
+@app.get("/items/{item_id}", response_model=Item)
+def get_item(item_id:int) -> Item:
     if item_id < len(items):
         item = items[item_id]
         return item
